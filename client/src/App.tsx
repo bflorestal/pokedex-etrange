10434b4ec108331f9f2fb81a8fff45dd979ff575
bflorestal <yayou7710@hotmail.fr>
2023-05-09 22:02:31 +0200

    Setup librairie pour composants

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import "./App.css";
 
 import type { PokemonWithId } from "../../server/src/modules/pokemon.schema";
 import { getAllPokemon } from "./utils/pokemon";
