6812a6cf629c8eb57a8389fc2184da798e862c41
bflorestal <yayou7710@hotmail.fr>
2023-05-09 20:32:36 +0200

    Implémentation CRUD

--- server/src/modules/pokemon.controller.ts
+++ server/src/modules/pokemon.controller.ts
@@ -5,7 +5,7 @@ import path from "path";
 import pokedex from "../data/pokedex.json";
 
 export const getAllPokemon = (_req: Request, res: Response) => {
-  res.status(200).json(pokedex);
+  return res.status(200).json(pokedex);
 };
 
 export const getPokemonById = (req: Request, res: Response) => {
@@ -13,14 +13,71 @@ export const getPokemonById = (req: Request, res: Response) => {
   const pokemon = pokedex.find((pokemon) => pokemon.id === id);
 
   if (!pokemon) {
-    res.status(404).json({ message: "Pokémon introuvable." });
+    return res.status(404).json({ message: "Pokémon introuvable." });
   }
 
-  res.status(200).json(pokemon);
+  return res.status(200).json(pokemon);
 };
 
-export const createPokemon = (req: Request, res: Response) => {};
+export const createPokemon = (req: Request, res: Response) => {
+  const pokemon = req.body;
 
-export const updatePokemon = (req: Request, res: Response) => {};
+  if (!pokemon) {
+    return res.status(400).json({ message: "Aucun Pokémon n'a été envoyé." });
+  }
 
-export const deletePokemon = (req: Request, res: Response) => {};
+  // TODO: Vérifier que le Pokémon n'existe pas déjà
+
+  // TODO: Vérifier que le Pokémon a bien toutes les propriétés requises
+
+  const newPokemon = { ...pokemon, id: pokedex.length + 1 };
+  pokedex.push(newPokemon);
+
+  fs.writeFileSync(
+    path.join(__dirname, "../data/pokedex.json"),
+    JSON.stringify(pokedex)
+  );
+
+  return res.status(201).json(newPokemon);
+};
+
+export const updatePokemon = (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  const pokemon = pokedex.find((pokemon) => pokemon.id === id);
+
+  if (!pokemon) {
+    return res.status(404).json({ message: "Pokémon introuvable." });
+  }
+
+  // TODO: Vérifier que les données envoyées sont valides
+
+  const updatedPokemon = { ...pokemon, ...req.body };
+  const index = pokedex.indexOf(pokemon);
+  pokedex[index] = updatedPokemon;
+
+  fs.writeFileSync(
+    path.join(__dirname, "../data/pokedex.json"),
+    JSON.stringify(pokedex)
+  );
+
+  return res.status(200).json(updatedPokemon);
+};
+
+export const deletePokemon = (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  const pokemon = pokedex.find((pokemon) => pokemon.id === id);
+
+  if (!pokemon) {
+    return res.status(404).json({ message: "Pokémon introuvable." });
+  }
+
+  const index = pokedex.indexOf(pokemon);
+  pokedex.splice(index, 1);
+
+  fs.writeFileSync(
+    path.join(__dirname, "../data/pokedex.json"),
+    JSON.stringify(pokedex)
+  );
+
+  return res.status(200).json({ message: "Pokémon supprimé." });
+};
