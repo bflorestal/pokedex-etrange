595daf94fce1f3f052ea72addb7dcb198e7cc30b
bflorestal <yayou7710@hotmail.fr>
2023-05-09 15:00:53 +0200

    Validation des variables d'environnement

---
+++ server/src/env.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const server = z.object({
+  PORT: z.number().int().positive().optional(),
+});
+
+const processEnv = {
+  PORT: Number(process.env.PORT),
+};
+
+const env = process.env as unknown as z.infer<typeof server>;
+
+const parsed = server.safeParse(processEnv);
+
+if (!parsed.success) {
+  console.error(
+    "❌ Variables d'environnement invalides :",
+    parsed.error.flatten().fieldErrors
+  );
+  throw new Error("Variables d'environnement invalides");
+}
+
+export { env };
